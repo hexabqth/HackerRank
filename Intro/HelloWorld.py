95b1c6f2ae0cc7f17c7e2b219fd8fd3bf21525e8
Abhishek Srivastava <abhishek.naresh.srivastava@gmail.com>
2022-08-06 21:09:34 +0530

    New Commits with HackerRank

--- Intro/HelloWorld.py
+++ Intro/HelloWorld.py
@@ -1,1 +1,9 @@
-print('Hello World')
+class HelloWorld:
+
+    def __init__(self) -> None:
+        pass
+    ref={'type':int,
+          'units':'Joules',
+          'docstring':'Test'  }
+    def check(self,**kwargs)->ref:
+        return 
